a4c6d58a8cf4f8a3c1405b06b3b50cb3e5370e2b
Hamsiga <srihamsiga.dev@gmail.com>
2026-02-24 19:02:46 +0530

    Bug fixed in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import taskRoutes from "./routes/taskRoutes.js";
+import autoTaskRoutes from "./routes/autoTaskRoutes.js";
+import alertRoutes from "./routes/alertRoutes.js";
+import environmentRoutes from "./routes/environmentRoutes.js";
+import predictiveRoutes from "./routes/predictiveRoutes.js";
+
+dotenv.config();
+connectDB();
 
 const app = express();
+app.use(express.json());
+
+// ROUTES
+app.use("/api/tasks", taskRoutes);
+app.use("/api/tasks/auto", autoTaskRoutes);
+app.use("/api/alerts", alertRoutes);
+app.use("/api/environment", environmentRoutes);
+app.use("/api/predictive", predictiveRoutes);
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
