197085022806833f38dc431b6f58aa7ffeab8489
Hamsiga <srihamsiga.dev@gmail.com>
2026-02-24 16:44:03 +0530

    Added route autoTaskRoutes.js

---
+++ backend/src/routes/autoTaskRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  getAutoTasks,
+  approve,
+  reject,
+} from "../controllers/autoTaskController.js";
+
+const router = express.Router();
+
+router.get("/", getAutoTasks);
+router.post("/:id/approve", approve);
+router.post("/:id/reject", reject);
+
+export default router;
