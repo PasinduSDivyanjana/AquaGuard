c358602c22d6d6fead9fb7bddbae11c3ef746d27
Hamsiga <srihamsiga.dev@gmail.com>
2026-04-07 19:45:10 +0530

    modified server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,16 +7,23 @@ import autoTaskRoutes from "./routes/autoTaskRoutes.js";
 import alertRoutes from "./routes/alertRoutes.js";
 import environmentRoutes from "./routes/environmentRoutes.js";
 import predictiveRoutes from "./routes/predictiveRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// Enable CORS
+app.use(cors());
+
 app.use(express.json());
 
 // ROUTES
-app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/tasks/auto", autoTaskRoutes);
+app.use("/api/tasks", taskRoutes);
 app.use("/api/alerts", alertRoutes);
 app.use("/api/environment", environmentRoutes);
 app.use("/api/predictive", predictiveRoutes);
@@ -28,3 +35,4 @@ app.use("/api/predictive", predictiveRoutes);
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
 });
+// Nodemon trigger
