9e8b816da0767a973efa9b07875f0a82e1acaaaf
Hamsiga <srihamsiga.dev@gmail.com>
2026-04-07 19:41:42 +0530

    modified environment routes

--- backend/src/routes/environmentRoutes.js
+++ backend/src/routes/environmentRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getWeather } from "../controllers/environmentController.js";
+import { getWeather, getAllWells } from "../controllers/environmentController.js";
 
 const router = express.Router();
 
+router.get("/wells", getAllWells);
 router.get("/weather/:wellId", getWeather);
 
 export default router;
